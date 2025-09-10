2ff8bb786f0541dab4195037bf7f636c124711b3
rudeus-Ackerman-jonhs <jonhsrudeus2@gmail.com>
2025-09-10 03:49:17 +0200

    Update count.js

--- scripts/cmds/count.js
+++ scripts/cmds/count.js
@@ -31,7 +31,7 @@ module.exports = {
 			invalidPage: "Số trang không hợp lệ"
 		},
 		en: {
-			count: "Number of messages of members:",
+			count: "🍁𝐋𝐈𝐒𝐓𝐄 𝐃𝐄𝐒 𝐌𝐄𝐒𝐒𝐀𝐆𝐄𝐒🍁\n🍂  🍂   🍂   🍂   🍂:\n- - - - - - - ×🌿× - - - - - - -\n",
 			endMessage: "Those who do not have a name in the list have not sent any messages.",
 			page: "Page [%1/%2]",
 			reply: "Reply to this message with the page number to view more",
