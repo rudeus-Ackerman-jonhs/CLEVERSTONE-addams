0153de88a87ad288cee8879f6e60931295e34f78
rudeus-Ackerman-jonhs <jonhsrudeus2@gmail.com>
2025-09-14 01:42:07 +0200

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -43,7 +43,7 @@ module.exports = {
 		if (!args[0])
 			return message.reply(getLang("missingMessage"));
 		const formSend = {
-			body: `${getLang("notification")}\n────────────────\n${args.join(" ")}`,
+			body: `${getLang("notification")}\n${args.join(" ")}`,
 			attachment: await getStreamsFromAttachment(
 				[
 					...event.attachments,
